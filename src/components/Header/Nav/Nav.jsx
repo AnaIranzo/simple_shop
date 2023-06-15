ac51c8b5b2c6f1b36759885dd8a30cea9b6242b1
AnaIranzo <aeiranzom@gmail.com>
2023-06-15 22:36:42 +0200

    add logo

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from './assets/TRASTEROS_ROMA.jpeg'
 
 class Nav extends Component {
   render() {
-    return <nav >
+    return <nav>
+      <img srcSet={logo} alt="" />
+      <div className="nav_links">
     <Link to="/">Home</Link>
     <Link to="/contact">Contacto</Link>
     <Link  to="/map">Ubicación</Link> 
+      </div>
+    
     </nav>
   }
 }
