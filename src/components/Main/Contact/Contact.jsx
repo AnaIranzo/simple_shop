cb2fc91f6913d2bd74f30725869d630d2454fec1
AnaIranzo <aeiranzom@gmail.com>
2023-06-25 14:44:22 +0200

    add formsubmit

--- src/components/Main/Contact/Contact.jsx
+++ src/components/Main/Contact/Contact.jsx
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-    return <form>
-      
-      <input className='contact_form' type="text"  placeholder='Nombre'/>
-      
-      
-      <input className='contact_form' type="email" placeholder='Email'/>
+    return <form action="https://formsubmit.co/aeiranzom@gmail.com" method="POST" >
 
-      <textarea className='contact_form' name="" id="" cols="30" rows="10" placeholder='Mensaje'></textarea>
-      <input type="submit" />
+      <input className='contact_form' type="text"  placeholder='Nombre' name="name" required/>
+      
+      
+      <input className='contact_form' type="email" placeholder='Email' name="email" required/>
+
+      <textarea className='contact_form' name="message" id="" cols="30" rows="10" placeholder='Mensaje' required></textarea>
+      <button type="submit">Enviar</button>
     </form>;
   }
 }
