5d44fe189e5031fbb9f54cc60cc54e66798a95d8
AnaIranzo <aeiranzom@gmail.com>
2023-06-28 12:49:36 +0200

    add markers

--- src/components/Main/Map/Map.jsx
+++ src/components/Main/Map/Map.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
 import { Icon } from "leaflet";
-
+const position = [38.920396, -6.326384]
 class Map extends Component {
+  
   render() {
     return <div id="map">
-    <MapContainer center={[38.920396, -6.326384]} zoom={120}scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={16}scrollWheelZoom={false}>
       <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={position}>
+      <Popup>Calle De Los Transportes Nave 7. CP 06800 Mérida</Popup>
+      <Tooltip>Estamos aquí</Tooltip>
+    </Marker>
     </MapContainer>
     </div>;
   }
